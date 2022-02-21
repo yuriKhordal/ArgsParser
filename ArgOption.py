a0dc8b208abca6bc3404f1e3d43212c24e145ee7
Yuri Khordal <the1pi1na4@gmail.com>
2022-02-21 22:15:47 +0200

    Added ArgOption and ArgFlag
    Added classes for ArgOption, an option in arguments, and ArgFlag, a flag
    option in arguments.
    
    + ArgOption: Added ArgOption class, that repreents an option in an
    argument list. Added ArgFlag class that inherits from ArgOption and
    represents an option that is a flag.
      __init__.py: Added exports for ArgOption and ArgFlag.

---
+++ ArgOption.py
@@ -0,0 +1,69 @@
+from ArgValsEnum import ArgValsEnum
+
+class ArgOption:
+  """Represents a command line option/flag."""
+
+  def __init__(self) -> None:
+    """Initializes a new argument."""
+    self.__short = ''
+    """
+    The short version of the option. Without `-`.
+    Type: `str`.
+    """
+    self.__long = ''
+    """
+    The long version of the option. Without `--`.
+    Type: `str`.
+    """
+    self.__require = ArgValsEnum.NONE
+    """
+    Whether the option requires extra arguments.
+    Type: `ArgValsEnum`.
+    """
+  
+  @property
+  def short(self) -> str:
+    """The short verion of the option."""
+    return self.__short
+
+  @property
+  def long(self) -> str:
+    """The long version of the option."""
+    return self.__long
+
+  @property
+  def valuesRequired(self) -> ArgValsEnum:
+    """The amount of values required by the argument."""
+    return self.__require
+
+  def setShort(self, short: str) -> ArgOption:
+    """Set the short version of the option."""
+    if not isinstance(short, str):
+      raise TypeError(short)
+    if len(short) != 1:
+      raise ValueError(f"Length of 'short' is {short.length}," + \
+        " expected a single character.")
+    self.__short = short
+    return self
+
+  def setLong(self, long: str) -> ArgOption:
+    """Set the long version of the option."""
+    if not isinstance(long, str):
+      raise TypeError(long)
+    self.__long = long
+    return self
+
+  def setValuesRequired(self, required: ArgValsEnum) -> ArgOption:
+    """."""
+    if not isinstance(required, ArgValsEnum):
+      raise TypeError(required)
+    self.__require = required
+    return self
+
+class ArgFlag(ArgOption):
+  """Represents a command line flag."""
+
+  def __init__(self) -> None:
+    """Initializes a new flag."""
+    super().__init__()
+    self.__require = ArgValsEnum.NONE
--- __init__.py
+++ __init__.py
@@ -1,1 +1,2 @@
 from ArgValsEnum import ArgValsEnum
+from ArgOption import ArgOption, ArgFlag
